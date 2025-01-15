6d092508a227cc683c6c7583fa207e672ec26f8e
Liam Appleton <liamappleton97@outlook.com>
2025-01-15 10:27:59 +0000

    adjustments to post route, now either adds new object to db or returns existing short url

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -2,15 +2,13 @@ const Joi = require("joi");
 const mongoose = require("mongoose");
 const express = require("express");
 const router = express.Router();
-const { randomKeyGenerator } = require("./utils");
-const urlSignature = "shortylinks.io/"; // figure out how to return a new link, add checking logic to see if link is already in db
+const { urlGenerator } = require("./utils");
 
 const Urls = mongoose.model(
   "Urls",
   mongoose.Schema({
     link: { type: String, required: true },
     shortUrl: String,
-    key: String,
   })
 );
 
@@ -26,23 +24,27 @@ router.get("/:id", async (req, res) => {
 });
 
 router.post("/", async (req, res) => {
-  const urlCheck = await Urls.findById(req.params.id);
-  let url;
+  try {
+    let exisitingUrl = await Urls.findOne({ link: req.body.link });
 
-  // if url does not exist, it is added to DB with a new key
-  if (!urlCheck) {
-    const key = randomKeyGenerator();
-    const shortenedUrl = urlSignature + key;
-    url = new Urls({
-      ...url,
-      link: req.body.link,
-      shortUrl: shortenedUrl,
-      key: key,
-    });
-    url = await url.save();
+    // if url does not exist, it is added to DB with a new key
+    if (!exisitingUrl) {
+      const shortenedUrl = urlGenerator();
+      exisitingUrl = new Urls({
+        ...exisitingUrl,
+        link: req.body.link,
+        shortUrl: shortenedUrl,
+      });
+
+      exisitingUrl = await exisitingUrl.save();
+      return res.send(exisitingUrl);
+    } else {
+      return res.send(exisitingUrl.shortUrl);
+    }
+  } catch (error) {
+    // update this block to account for invalid URLS etc...
+    res.status(404).send(error.message);
   }
-
-  res.send(url);
 });
 
 module.exports = router;
--- server/routes/utils.js
+++ server/routes/utils.js
@@ -1,13 +1,13 @@
-const randomKeyGenerator = () => {
+const urlGenerator = () => {
   const chars =
     "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
   let key = [];
   for (let i = 0; i < 16; i++) {
     key.push(chars[Math.floor(Math.random() * chars.length)]);
   }
-  return key.join("");
+  return `shortylinks.io/${key.join("")}`;
 };
 
 module.exports = {
-  randomKeyGenerator,
+  urlGenerator,
 };
